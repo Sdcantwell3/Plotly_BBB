ee0ec2f873fae99347476462847c1519ff63f81c
Sdcantwell3 <sdcantwell2@gmail.com>
2022-09-10 18:14:00 -0600

    Challenge Work

--- charts.js
+++ charts.js
@@ -108,7 +108,7 @@ function buildCharts(sample) {
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: "Sunset"
+        colorscale: "Portland"
       }
    
     }];
